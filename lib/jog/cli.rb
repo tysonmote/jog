009bd9dc4dc0f9ac814eba74d978093d774e569c
Tyson Tate <tyson@tysontate.com>
2012-09-06 10:22:40 -0700

    Add 'yesterday' option. Bump version.

--- lib/jog/cli.rb
+++ lib/jog/cli.rb
@@ -1,12 +1,18 @@
 require 'thor'
 require 'pp'
 require 'colored'
+require 'active_support/time'
 
 module Jog
   class CLI < Thor
     desc "today", "Edit the log entry for today"
     def today
       Jog.edit( Jog.get_or_create_file( Time.now ) )
+    end
+
+    desc "yesterday", "Edit the log entry for yesterday"
+    def yesterday
+      Jog.edit( Jog.get_or_create_file( Time.now - 1.day ) )
     end
 
     desc "config", "Print the current configuration"
